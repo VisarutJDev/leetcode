91f88717cf19383add44287800c20d259c355e45
Visarut Junsone <visarutjwork@gmail.com>
2021-04-12 16:50:26 +0700

    practice

--- practice/linked_list.go
+++ practice/linked_list.go
@@ -1,11 +1,19 @@
 package practice
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // NodeString is element for linked link node string
 type NodeString struct {
 	Data string
 	Next *NodeString
+}
+
+// NodeInt is element to hold node int
+type NodeInt struct {
+	Data int
+	Next *NodeInt
 }
 
 // AppendNode to append node string
@@ -59,4 +67,70 @@ func (n *NodeString) KthToElement(k int) []string {
 	}
 
 	return arr[k-1:]
+}
+
+// GetValue is a function to get value from node int
+func GetValue(l *NodeInt) int {
+	unit := 10
+	c := l
+	sum := c.Data
+	for c.Next != nil {
+		sum += c.Next.Data * unit
+		unit *= 10
+		c = c.Next
+	}
+	return sum
+}
+
+// Add is a function to add 2 node int
+func Add(l1, l2 *NodeInt) int {
+	return GetValue(l1) + GetValue(l2)
+}
+
+// AppendNode to append node int
+func (n *NodeInt) AppendNode(s int) {
+	newNode := &NodeInt{Data: s}
+	currentNode := n
+	for currentNode.Next != nil {
+		currentNode = currentNode.Next
+	}
+	currentNode.Next = newNode
+}
+
+// GetValueRe is a function to get value from node int
+func GetValueRe(l *NodeInt) int {
+	var arr []int
+	unit := 1
+	c := l
+	arr = append(arr, c.Data)
+	sum := 0
+	for c.Next != nil {
+		arr = append(arr, c.Next.Data)
+		unit *= 10
+		c = c.Next
+	}
+	for i := range arr {
+		sum += arr[i] * unit
+		unit /= 10
+	}
+	return sum
+}
+
+// AddRe is a function to addRe 2 node int
+func AddRe(l1, l2 *NodeInt) int {
+	return GetValueRe(l1) + GetValueRe(l2)
+}
+
+func (n *NodeString) CheckNodeLoop() string {
+	check := make(map[string]bool)
+	check[n.Data] = true
+	c := n
+	for c.Next != nil {
+		if check[c.Next.Data] {
+			return c.Next.Data
+		}
+		check[c.Next.Data] = true
+		c = c.Next
+	}
+	return ""
 }
--- practice/linked_list_test.go
+++ practice/linked_list_test.go
@@ -22,3 +22,32 @@ func TestRemoveDup(t *testing.T) {
 	// fmt.Println(node.KthToElement(7))
 	fmt.Println(node.KthToElement(3))
 }
+
+func TestAdd(t *testing.T) {
+	node1 := &NodeInt{Data: 7}
+	node1.AppendNode(1)
+	node1.AppendNode(6)
+	node2 := &NodeInt{Data: 5}
+	node2.AppendNode(9)
+	node2.AppendNode(2)
+	fmt.Println(Add(node1, node2))
+}
+func TestAddRe(t *testing.T) {
+	node1 := &NodeInt{Data: 6}
+	node1.AppendNode(1)
+	node1.AppendNode(7)
+	node2 := &NodeInt{Data: 2}
+	node2.AppendNode(9)
+	node2.AppendNode(5)
+	fmt.Println(AddRe(node1, node2))
+}
+
+func TestCheckNodeLoop(t *testing.T) {
+	node := &NodeString{Data: "A"}
+	node.AppendNode("B")
+	node.AppendNode("C")
+	node.AppendNode("D")
+	node.AppendNode("E")
+	node.AppendNode("C")
+	fmt.Println(node.CheckNodeLoop())
+}
